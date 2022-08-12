9007f8764d1059bfb6b1b21d92efd32848119706
BulychevMikhail <102511691+BulychevMikhail@users.noreply.github.com>
2022-08-12 20:18:54 +0400

    Update test_all_pets_are_present.py

--- tests/test_all_pets_are_present.py
+++ tests/test_all_pets_are_present.py
@@ -1,27 +1,4 @@
 from conftest import *
-#
-#
-# def test_all_pets_are_present(go_to_my_pets):
-#     """Проверяем что на странице со списком моих питомцев присутствуют питомцы"""
-#
-#     # Сохраняем в переменную statistic элементы статистики
-#     statistic = pytest.driver.find_elements(By.CSS_SELECTOR, ".\\.col-sm-4.left")
-#
-#     # Сохраняем в переменную pets элементы карточек питомцев
-#     pets = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
-#
-#     # Получаем количество питомцев из данных статистики
-#     number = statistic[0].text.split('\n')
-#     number = number[1].split(' ')
-#     number = int(number[1])
-#
-#     # Получаем количество карточек питомцев
-#     number_of_pets = len(pets)
-#
-#     # Проверяем что количество питомцев из статистики совпадает с количеством карточек питомцев
-#     assert number == number_of_pets
-#     assert number == 19
-#     assert number_of_pets == 19
 
 
 def test_all_pets_are_present(go_to_my_pets):
